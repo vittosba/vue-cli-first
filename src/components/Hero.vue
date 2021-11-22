01631cf06d5a8d9c6da14912dd303904e87c6d32
vittosba <vitsbardella@gmail.com>
2021-11-22 17:23:27 +0100

    add hero

---
+++ src/components/Hero.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="hero">
+      <h2>E qui siamo dentro l'hero</h2>
+  </section>
+</template>
+
+<script>
+export default {
+    name:'Hero',
+}
+</script>
+
+<style>
+    .hero {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 400px;
+        background: rgb(16, 221, 50);
+    }
+</style>
