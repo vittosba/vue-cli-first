26c353d6d2619eb4bc3c52b532ee91d68943ceae
vittosba <vitsbardella@gmail.com>
2021-11-22 17:18:26 +0100

    add title

---
+++ src/components/Title.vue
@@ -0,0 +1,15 @@
+<template>
+    <section class="title">
+        <h1>La mia prima app con Vue CLI!</h1>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Title',
+}
+</script>
+
+<style>
+
+</style>
